0998524e8670b176ec97976624b718582a8fe9fc
Unknown <woontek9403@naver.com>
2018-05-28 11:21:38 +0900

    javascript 1회차 :변수 string 예제
    javascript 1회차 :변수 string 예제

---
+++ javascript/day1/js/variable_string.js
@@ -0,0 +1,28 @@
+console.log("hell java");
+
+var str1 = "my name is";
+var str2 = 'javascriput';
+var str3 = String("my name is");
+var str4 = new String("javascript");
+var str5 = str2.valueOf();
+
+console.log("str1 : "+str1);
+console.log("str2 : "+str2);
+console.log("str3 : "+str3);
+console.log("str4 : "+str4);
+console.log("str5 : "+str5);
+
+console.log("str1 type: "+typeof str1);
+console.log("str2 type: "+typeof str2);
+console.log("str3 type: "+typeof str3);
+console.log("str4 type: "+typeof str4);
+console.log("str5 type: "+typeof str5);
+console.log("문자열 비교")
+
+
+console.log("A" > "B");
+console.log("A" > "AB");
+console.log("A" > "a");
+console.log("3" > "10");
+console.log(3 > "10");
+console.log(3 > 10);
