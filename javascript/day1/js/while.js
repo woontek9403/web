e0073f85e1663c07a0d48b563cc06b700ca00348
Unknown <woontek9403@naver.com>
2018-05-28 15:05:42 +0900

    javascript 1회차 : 반복문 for문 do~while 문 while 문예제
    javascript 1회차 : 반복문 3개예제

---
+++ javascript/day1/js/while.js
@@ -0,0 +1,46 @@
+
+
+// 초기화;
+// while(조건식){
+//     실행문;
+//     증감연산;
+// }
+// 1~10까지의 증감연산 
+var i =1;
+var sum = 0;
+while(i <=10){
+    console.log("i : " +i);
+    sum += i; // sum= sum + i;
+    i++;
+}
+console.log("sum : " +sum);
+
+
+// 초기화;
+// do{
+//     실행문;
+//     증감연산;
+// }
+// while(조건식);
+i = 11;
+sum = 0;
+do{
+    console.log("i : " +i);
+    sum + i;
+    i++;
+}
+while(i<=10);
+console.log("sum : " + sum);
+
+
+
+
+
+// for(초기화; 조건식; 증감연산){
+//     실행문;
+// }
+
+// i=1;
+// for(i=1; i<=10; i++){
+//     console.log("i : " + i);
+// }
