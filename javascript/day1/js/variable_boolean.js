e0570cecb1790cf519a0873fc8040589448ba332
Unknown <woontek9403@naver.com>
2018-05-28 11:37:47 +0900

    javascript 1회차 : 변수 boolean예제 + undefined예제제
    javascript 1회차 : 변수 boolean예제 + undefined예제

---
+++ javascript/day1/js/variable_boolean.js
@@ -0,0 +1,17 @@
+var bool1 = true;
+var bool2 = 1; 
+var bool3 = 0;
+var bool4 =  "";
+var bool5 = null;
+var bool6 = NaN;
+
+console.log(!!bool1);
+console.log(!!bool2);
+console.log(!!bool3);
+console.log(!!bool4);
+console.log(!!bool5);
+console.log(!!bool6);
+// NaN 과 NaN 을 붙히는 이유는 
+
+var bool7;
+console.log("bool7 : " +bool7);
