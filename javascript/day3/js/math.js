1fede4083015f320c2e3de268c989d03e4ed8bdd
Unknown <woontek9403@naver.com>
2018-06-04 12:33:13 +0900

    javascript 3회차 : math 예제

---
+++ javascript/day3/js/math.js
@@ -0,0 +1,10 @@
+console.log("Math.PI : "+Math.PI);
+console.log("Math.E : "+Math.E);
+console.log("Math.pow(2,3) : " + Math.pow(2,3));
+console.log("Math.sqrt(4) : " + Math.sqrt(4));
+console.log("Math.ceil(1.3) : " +Math.ceil(1.3));
+console.log("Math.min(1.4) " + Math.min(1.4));
+console.log("Math.max(1.4) " + Math.max(1.4));
+console.log("Math.random() " + Math.random());
+console.log("Math.round(1.3) : " + Math.round(1.3));
+console.log("Math.floor(1.9) " + Math.floor(1.9));
