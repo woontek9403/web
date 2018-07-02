4f7e612439c79898f48b3e1e710298570d27981b
woontek9403 <38845416+woontek9403@users.noreply.github.com>
2018-07-02 15:19:21 +0900

    자바 스크립트 예제 모음

--- javascript/day6/js/stopImmediatePropagationEx.js
+++ javascript/day6/js/stopImmediatePropagationEx.js
@@ -8,6 +8,7 @@ function fnload(){
     var depth1 = document.getElementById('depth1');
     var depth2 = document.getElementById('depth2');
     var depth3 = document.getElementById('depth3');
+    var depth4 = document.getElementById('depth4');
 
     depth1.addEventListener('click' , function(e){
         console.log(eventPhase[e.eventPhase] +' : depth1');
@@ -18,7 +19,19 @@ function fnload(){
         },false);
 
     depth3.addEventListener('click' , function(e){
-        console.log(eventPhase[e.eventPhase] +' : depth3');
+        console.log(eventPhase[e.eventPhase] +' : depth3 : first');
         e.stopPropagation();
+        },false);
+    depth3.addEventListener('click' , function(e){
+        console.log(eventPhase[e.eventPhase] +' : depth3 : second');
+        e.stopPropagation();
+        },false);
+    depth4.addEventListener('click' , function(e){
+        console.log(eventPhase[e.eventPhase] +' : depth4 : first');
+        e.stopImmediatePropagation();
+        },false);
+    depth4.addEventListener('click' , function(e){
+        console.log(eventPhase[e.eventPhase] +' : depth4 : second');
+        e.stopImmediatePropagation();
         },false);
 }
