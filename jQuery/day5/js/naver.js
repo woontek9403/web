8302111204155eb884d68064c6974970ba7b97c3
Unknown <woontek9403@naver.com>
2018-07-17 17:19:26 +0900

    jquery/naver예제

---
+++ jQuery/day5/js/naver.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $("#li").mouseenter(function(){
+        $(".hi").show();    
+    });
+    $("#li").mouseleave(function(){
+        $(".hi").hide();
+    });
+});
